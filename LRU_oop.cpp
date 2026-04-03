fdfa6bf3ffb70d8ae73c63fc42f6cbb5bc4d06d8
dariesan29 <darius.ariesan@yahoo.com>
2026-04-03 14:28:00 +0300

    Update LRU_oop.cpp
    Fixed version

--- LRU_oop.cpp
+++ LRU_oop.cpp
@@ -6,7 +6,7 @@
 class CacheLRU{
 private:
    std::list<std::pair<int, char>> LRU;
-   std::unordered_map<int, std::list<std::pair<int, char>>::iterator> list_it;
+   std::unordered_map<int, std::list<std::pair<int, char>>::iterator> MAP_it;
    int max = 3;
 
    public:
@@ -15,28 +15,28 @@ private:
 
    void put(int key, char value)
    {
-        auto it = list_it.find(key);
-        if(it != list_it.end())
+        auto it = MAP_it.find(key);
+        if(it != MAP_it.end())
             {
                 it->second->second = value;
                 LRU.splice(LRU.begin(), LRU, it->second);
             }
         else{
-            if(LRU.size() > max)
+            if(LRU.size() == max)
                 {
                     auto last_key = LRU.back();
-                    list_it.erase(last_key.first);
+                    MAP_it.erase(last_key.first);
                     LRU.pop_back();
                 }
             LRU.emplace_front(key, value);
-            list_it[key] = LRU.begin();
+            MAP_it[key] = LRU.begin();
         }
    }
 
    char get(int key)
     {   
-        auto it = list_it.find(key);
-        if(it != list_it.end())
+        auto it = MAP_it.find(key);
+        if(it != MAP_it.end())
             {
             LRU.splice(LRU.begin(), LRU, it->second );
             return it->second->second;
@@ -44,22 +44,19 @@ private:
         return -1;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const CacheLRU& LRU);
+    friend std::ostream& operator<<(std::ostream& os,CacheLRU& LRU);
 };
 
-    std::ostream& operator<<(std::ostream& os, const CacheLRU& list){
-        for (auto um : list.LRU)
-        {
-            for (auto pair : um)
+    std::ostream& operator<<(std::ostream& os,CacheLRU& list){
+        for (auto pair : list.LRU)
             os<<pair.first<<" "<<pair.second<<std::endl;
-        }
         os<<std::endl;
         return os;
     }
 
 int main(){
 
-    CacheLRU LRU{3};
+    CacheLRU LRU{4};
 
     LRU.put(1, 'A');
     LRU.put(2, 'B');
